fc255116610d3d80f6490231d4ca0af5e499a448
khireddinemahaline <khirozr@gmail.com>
2024-03-07 00:16:48 -0800

    0-sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sort.h>
+/**
+ *
+ *
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	for (i = 0; i < array.length - 1; i++)
+	{
+		for (j = 0; j < array.length - i - 1; j++ )
+		{
+			if array[j] > array[j + 1];
+			int reg = array[j];
+			array[j + 1] = array[j];
+			array[j] = reg;
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,10 @@
+#define HEADER
+#ifndef HEADER
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+
+
+#endif

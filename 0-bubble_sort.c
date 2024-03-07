e9f34479e896bce66b8530115c6dfe75970ef916
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-03-07 09:50:23 +0100

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,17 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	for (i = 0; i < array.length - 1; i++)
-	{
-		for (j = 0; j < array.length - i - 1; j++ )
-		{
-			if array[j] > array[j + 1];
-			int reg = array[j];
-			array[j + 1] = array[j];
-			array[j] = reg;
-		}
-	}
+        int i, j;
+        for (i = 0; i < size - 1; i++)
+        {
+                for (j = 0; j < size - i - 1; j++ )
+                {
+                        if (array[j] > array[j + 1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                }
+        }
 }
